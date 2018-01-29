895091468ffe47815f2c5f9e6280237e119e1035
wolfm2@newschool.edu <wolfm2@newschool.edu>
2018-01-29 16:00:48 -0500

    simplified code

--- fboFadingSimple/src/ofApp.cpp
+++ fboFadingSimple/src/ofApp.cpp
@@ -1,22 +1,19 @@
 #include "ofApp.h"
 
-ofFbo fbo, spFbo;  // our regular fbo and "scratchpad" fbo
-ofColor ball(100,0,200);
+// Track mouse with fade
+
+ofFbo fbo;  // framebuffer object
+ofColor ball(100,0,200);  // purple
 ofColor black(0,0,0);
 
 //--------------------------------------------------------------
 void ofApp::setup(){
   //fbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA);  // lower precision. leaves artifacts. way faster.
   fbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA32F); // higher precision alpha (no artifacts)
-  spFbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA32F);   // not doing any blending here.  no alpha needed 
   
   fbo.begin();
     ofClear(255,255,255, 0);
   fbo.end();
-  
-  spFbo.begin();
-    ofClear(255,255,255, 0);
-  spFbo.end();
   
   ofSetBackgroundColor(black);
 }
@@ -31,20 +28,13 @@ int mouseY = 0;
 //--------------------------------------------------------------
 void ofApp::draw(){
   fbo.begin();
-    ofSetColor(black, 10); // background color with alpha  / change color for halo effect
+    ofSetColor(black, 10); // background color with alpha 
     ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
     ofSetColor(ball);
     ofNoFill();
     ofDrawCircle(mouseX, mouseY, 50);
     ofFill();
   fbo.end();
-  
-  spFbo.begin();  // write the existing fbo to the scratchpad but a bit smaller
-    ofSetColor(255,255,255);  // always set the color before drawing the fbo
-    fbo.draw(10, 10, ofGetWidth()-20, ofGetHeight()-20);
-  spFbo.end(); 
-  
-  fbo = spFbo;    // write the scratchpad back to the original
   
   ofSetColor(255,255,255);  // always set the color before drawing the fbo
   fbo.draw(0,0);
